dfcd29476ae615261fbcc4f78780bb85b3db2f71
liushy <l.shynkewich@gmail.com>
2026-04-02 16:42:09 +0300

    base test fixture added

---
+++ fixtures/baseTest.js
@@ -0,0 +1,21 @@
+import { test as baseTest} from '@playwright/test'
+import { HomePage } from '..pages/HomePage';
+import { AuthPage } from '..pages/AuthPage';
+import { DashboardPage } from '..pages/DashboardPage';
+
+export const baseTest = baseTest.extend({
+    homePage: async ({ page }, use) => {
+        const homePage = new HomePage(page);
+        await use(homePage);
+    },
+    authPage: async ({ page }, use) => {
+        const authPage = new AuthPage(page);
+        await use(authPage);
+    },
+    dashboardPage: async ({ page }, use) => {
+        const dashboardPage = new DashboardPage;
+        await use(dashboardPage);
+    }
+});
+
+export { expect } from '@playwright/test';
